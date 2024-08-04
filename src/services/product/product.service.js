8607b209c42d0e21bb510c795365621fbff9cb3f
longdv ph29867 <Vuthimien003@gmail.com>
2024-08-04 21:08:59 +0700

    update api product

--- src/services/product/product.service.js
+++ src/services/product/product.service.js
@@ -1,135 +1,108 @@
 import Product from "../../models/Product/Product.model.js";
 import ApiError from "../../utils/ApiError.js";
 import httpStatus from "http-status";
+import attributeService from "./attribute.service.js";
 
-const getAllProducts = async(filter, options) => {
-    const products = await Product.paginate(filter, options);
-    return products;
+const getAllProducts = async (filter, options) => {
+  const products = await Product.paginate(filter, options);
+  return products;
 };
 
-const getProductByID = async(idProduct) => {
-    const product = await Product.findOne({ _id: idProduct })
-        .populate("attributes")
-        .populate({ path: "categories", select: "-active" })
-        .select("-active");
-    return product;
+const getProductById = async (id) => {
+  const product = await Product.findOne({ _id: id })
+    .populate({ path: "attributes", populate: "values" })
+    .populate({ path: "categories", select: "-active" })
+    .select("-active");
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
+  }
+  return product;
 };
 
-const getProductBySlug = async(slugProduct) => {
-    const product = await Product.findOne({ slug: slugProduct })
-        .populate("attributes")
-        .populate({ path: "categories", select: "-active" })
-        .select("-active");
-    return product;
+// ///
+const getById = async (id) => {
+  const data = await Product.findById(id);
+  if (!data) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
+  }
+  return data;
 };
 
-const createProducts = async(bodyProduct) => {
-    if (await Product.isSlugTaken(bodyProduct.slug)) {
-        throw new ApiError(httpStatus.NOT_FOUND, "Products already exists");
-    }
-    const newAttrbutes = await attributeService.createAttributeMany(
-        bodyProduct.attributes
-    );
-    if (!newAttrbutes) {
-        throw new ApiError(httpStatus.NOT_FOUND, "Attribute create failed");
-    }
-    bodyProduct.minPrice = Math.min(
-        ...newAttrbutes.map((attr) => attr.discount == 0 ? attr.price : attr.discount)
-    );
-    bodyProduct.maxPrice = Math.max(
-        ...newAttrbutes.map((attr) => attr.discount == 0 ? attr.price : attr.discount)
-    );
-    const insertedIds = newAttrbutes.map((doc) => doc._id);
-    const dataProduct = {...bodyProduct, attributes: insertedIds };
-    const newProduct = await Product.create(dataProduct);
-    if (!newProduct) {
-        throw new ApiError(
-            httpStatus.INTERNAL_SERVER_ERROR,
-            "Create Products failed"
-        );
-    }
-    return newProduct;
+const getProductBySlug = async (slug) => {
+  const product = await Product.findOne({ slug })
+    .populate("attributes")
+    .populate({ path: "categories", select: "-active" })
+    .select("-active");
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
+  }
+  return product;
 };
 
-const updateProducts = async(idProduct, bodyProduct) => {
-    try {
-        if (await Product.isSlugTaken(bodyProduct.slug, idProduct)) {
-            throw new ApiError(httpStatus.BAD_REQUEST, "Products already exists");
-        }
-        const product = await Product.findById(idProduct);
-        if (!product) {
-            throw new ApiError(httpStatus.NOT_FOUND, "Products not found");
-        }
-
-        await attributeService.deleteAttributeMany(product.attributes);
-        const newAttrbutes = await attributeService.createAttributeMany(
-            bodyProduct.attributes
-        );
-        bodyProduct.minPrice = Math.min(
-            ...newAttrbutes.map((attr) => attr.discount == 0 ? attr.price : attr.discount)
-        );
-        bodyProduct.maxPrice = Math.max(
-            ...newAttrbutes.map((attr) => attr.discount == 0 ? attr.price : attr.discount)
-        );
-        const insertedIds = newAttrbutes.map((doc) => doc._id);
-        const dataProduct = {...bodyProduct, attributes: insertedIds };
-        const updatedProduct = await Product.findByIdAndUpdate(
-            idProduct, { $set: dataProduct }, { new: true, runValidators: true }
-        );
-
-        return updatedProduct;
-    } catch (error) {
-        console.error("Error updating product:", error);
-        throw error;
-    }
+const create = async (body) => {
+  if (await Product.isSlugTaken(body.slug)) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Product already exists");
+  }
+  return await Product.create(body);
 };
 
-const deleteProductById = async(productId) => {
-    const product = await getProductByID(productId);
-    if (!product) {
-        throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
-    }
-    await Product.findByIdAndUpdate(product.id, { active: false });
-    return product;
+const updateProduct = async (id, body) => {
+  const product = await getById(id);
+  if (body.slug && (await Product.isSlugTaken(body.slug, id))) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "Product already exists");
+  }
+  Object.assign(product, body);
+  await product.save();
+  return product;
 };
 
-const updateScoreReviewProduct = async(productId, score, type) => {
-    if (!["update", "delete"].includes(type)) {
-        throw new ApiError(httpStatus.BAD_REQUEST, "Invalid type specified");
-    }
-    const product = await Product.findById(productId);
-    if (!product) {
-        throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
-    }
+const deleteProductById = async (productId) => {
+  const product = await getProductById(productId);
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
+  }
+  await Product.findByIdAndUpdate(product.id, { active: false });
+  return product;
+};
 
-    let numReviews = product.numReviews;
-    let scoreReview = product.scoreReview;
+const updateScoreReviewProduct = async (productId, score, type) => {
+  if (!["update", "delete"].includes(type)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "Invalid type specified");
+  }
+  const product = await Product.findById(productId);
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Product not found");
+  }
 
-    if (type === "update") {
-        numReviews += 1;
-        scoreReview += score;
-    } else if (type === "delete") {
-        numReviews -= 1;
-        scoreReview -= score;
-        if (scoreReview < 0 || numReviews <= 0) {
-            numReviews = 0;
-            scoreReview = 0;
-        }
+  let numReviews = product.numReviews;
+  let scoreReview = product.scoreReview;
+
+  if (type === "update") {
+    numReviews += 1;
+    scoreReview += score;
+  } else if (type === "delete") {
+    numReviews -= 1;
+    scoreReview -= score;
+    if (scoreReview < 0 || numReviews <= 0) {
+      numReviews = 0;
+      scoreReview = 0;
     }
-    const finalScoreReview = numReviews === 0 ? 0 : scoreReview / numReviews;
-    product.numReviews = numReviews;
-    product.scoreReview = scoreReview;
-    product.finalScoreReview = finalScoreReview;
-    await product.save();
-    return product;
+  }
+  const finalScoreReview = numReviews === 0 ? 0 : scoreReview / numReviews;
+  product.numReviews = numReviews;
+  product.scoreReview = scoreReview;
+  product.finalScoreReview = finalScoreReview;
+  await product.save();
+  return product;
 };
 const productService = {
-    getAllProducts,
-    createProducts,
-    updateProducts,
-    getProductByID,
-    getProductBySlug,
-    deleteProductById,
-    updateScoreReviewProduct,
+  getAllProducts,
+  create,
+  getById,
+  updateProduct,
+  getProductById,
+  getProductBySlug,
+  deleteProductById,
+  updateScoreReviewProduct,
 };
 export default productService;
